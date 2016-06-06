9f07badca1f949baf4ec3e394c8c420052847b8f
Batandwa <i@batandwa.me>
2016-06-06 11:11:39 +0200

    Allow operation to be passed in as a parameter.

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ usage()
 cat << EOF
 usage: $0 
 
+operation  Optional. Operation to do. Valid commands are Migrate, Clean, Info, Validate, Baseline and Repair
+
 This script requires the following environment variables are set.
 
 OPTIONS:
@@ -21,6 +23,11 @@ EOF
 export MIGRATIONS_PATH=${MIGRATIONS_PATH:-'/home/flyway/migrations'}
 export DATABASE_PORT=${DATABASE_PORT:-3306}
 export DATABASE_ENCODING=${DATABASE_ENCODING:-UTF-8}
+export OPERATION=$1
+
+if [[ -z $1 ]]; then
+    export OPERATION=migrate
+fi
 
 if [[ -z $DATABASE_NAME ]] || [[ -z $DATABASE_USER ]] || [[ -z $DATABASE_DRIVER ]] || [[ -z $MIGRATIONS_PATH ]]; then
     usage
@@ -29,5 +36,5 @@ fi
 
 while ! nc db $DATABASE_PORT </dev/null; do sleep 10; done
 
-flyway -url=jdbc:$DATABASE_DRIVER://db:$DATABASE_PORT/$DATABASE_NAME -user=$DATABASE_USER -password=$DATABASE_PASSWORD -encoding=$DATABASE_ENCODING -locations=filesystem:$MIGRATIONS_PATH migrate
+flyway -url=jdbc:$DATABASE_DRIVER://db:$DATABASE_PORT/$DATABASE_NAME -user=$DATABASE_USER -password=$DATABASE_PASSWORD -encoding=$DATABASE_ENCODING -locations=filesystem:$MIGRATIONS_PATH $OPERATION
 
